60d09f45429f65227642afb0622b4206fe8fdc89
Alain Rodriguez <eagle5command@gmail.com>
2013-02-22 11:29:38 -0800

    Added tests for GeofencedGroup

---
+++ test/test_geofenced_group.js
@@ -0,0 +1,40 @@
+var expect = require('chai').expect;
+
+var utils = require('../lib/utils');
+var Geofence = require('../lib/geofence');
+var GeofencedGroup = require('../lib/geofenced_group');
+
+var gfg = new GeofencedGroup();
+
+var wogf1 = new Geofence([[10,10], [100,10], [100,50], [10,50]]);
+var bogf1 = new Geofence([[20,20], [40,20], [40,30], [20,30]]);
+var bogf2 = new Geofence([[50,22], [60,22], [60,26], [50,26]]);
+
+var wogf2 = new Geofence([[200,5], [300,5], [300,100], [200,100]]);
+
+gfg.add(1, [wogf1, wogf2], [bogf1, bogf2]);
+gfg.add(2, [wogf1]);
+gfg.add(3, null, null);
+gfg.add(4, null, [bogf2]);
+
+describe('GeofencedGroup.getValid()', function() {
+    it('a point far out should include 3, 4', function() {
+        expect(gfg.getValid(1000,1000)).to.deep.equal([3, 4]);
+    });
+
+    it('a point in whiteout 1 should include 1, 2, 3', function() {
+        expect(gfg.getValid([15, 15])).to.deep.equal([1, 2, 3, 4]);
+    });
+
+    it('a point in whiteout 2 should include 2, 3', function() {
+        expect(gfg.getValid([250, 10])).to.deep.equal([1, 3, 4]);
+    });
+
+    it('a point in blackout 1 should include 2, 3', function() {
+        expect(gfg.getValid([25, 25])).to.deep.equal([2, 3, 4]);
+    });
+
+    it('a point in blackout 2 should include 2, 3', function() {
+        expect(gfg.getValid([55, 25])).to.deep.equal([2, 3]);
+    });
+});
